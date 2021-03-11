b6d4cf3a8833dcb86b290a3f7d849a5d4e57b221
Arthur Falcão <arthurjvfalcao@gmail.com>
2021-03-11 21:16:11 +0000

    :rocket: add support to prettier

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
-import { PolymerElement, html } from '@polymer/polymer';
-import { customElement } from '@polymer/decorators';
+import { PolymerElement, html } from '@polymer/polymer'
+import { customElement } from '@polymer/decorators'
 
 @customElement('polymer3-app')
 class App extends PolymerElement {
   static get template() {
-    return html`
-      <h1>Polymer3 Playground</h1>
-    `;
+    return html` <h1>Polymer3 Playground</h1> `
   }
 }
 
-export default App;
+export default App
