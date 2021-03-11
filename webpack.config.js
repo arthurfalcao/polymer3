f4bfb0b90d63ea80c394545e07592cd5c00c7e28
Arthur Falcão <arthurjvfalcao@gmail.com>
2021-03-11 00:33:51 +0000

    :arrow_up: update webpack deps and config

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,44 @@
 const path = require('path');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const webpack = require('webpack');
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: [
-        'webpack-dev-server/client?http://localhost:8080',
-        './src/app'
-    ]
-  },
+  mode: 'development',
+  entry: './src/app.ts',
   devtool: 'inline-source-map',
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
-    hot: true
+    hot: true,
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, '..', 'dist')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
-    rules: [
-      {
-        test: /\.(html)$/,
-        use: {
-          loader: 'html-loader'
-        }
-      },
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+    rules: [{ test: /\.ts?$/, use: 'ts-loader' }],
   },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
   },
   plugins: [
-    new CleanWebpackPlugin(['dist'], { verbose: true, root: path.resolve(__dirname) }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
+    new HtmlPlugin({
+      template: 'public/index.html',
     }),
-    new CopyWebpackPlugin([
-      {
-        from: path.join(
-          path.resolve(__dirname, './node_modules/@webcomponents/webcomponentsjs/'),
-          '*.js'
-        ),
-        to: './webcomponentjs',
-        flatten: true
-      }
-    ]),
-    new webpack.IgnorePlugin(/vertx/),
-    new webpack.HotModuleReplacementPlugin(),
-  ]
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.join(
+            path.resolve(
+              __dirname,
+              'node_modules/@webcomponents/webcomponentsjs/'
+            ),
+            '*.js'
+          ),
+          to: 'webcomponentsjs/[name][ext]',
+        },
+      ],
+    }),
+  ],
 };
