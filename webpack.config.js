abe7ab7cbd3d606e8ea34fa6422473d9834bf1bf
Arthur Falcão <arthurjvfalcao@gmail.com>
2021-03-11 00:37:21 +0000

    feature: typescript (#7)
    * :rocket: add support to typescript
    
    * :arrow_up: update webpack deps and config
    
    * :memo: update main file in the package.json

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.ts',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true,
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [{ test: /\.ts?$/, use: 'ts-loader' }],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: 'public/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.join(
+            path.resolve(
+              __dirname,
+              'node_modules/@webcomponents/webcomponentsjs/'
+            ),
+            '*.js'
+          ),
+          to: 'webcomponentsjs/[name][ext]',
+        },
+      ],
+    }),
+  ],
+};
