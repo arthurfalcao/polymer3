201def14f62f4b394f5cde05980f7ac4088b2baa
Arthur Falcão <arthurjvfalcao@gmail.com>
2021-04-03 19:22:31 +0100

    chore: test fixture (#20)
    * 🚀 implement template fixture and improve component render
    
    * 🔥 remove @open-wc/testing

--- src/app.test.ts
+++ src/app.test.ts
@@ -3,15 +3,13 @@ import { render } from './utils/tests/helpers'
 import './app'
 
 describe('<polymer3-app />', () => {
-  it('should render the heading', async () => {
-    const { container, getByRole } = await render(
-      `<polymer3-app></polymer3-app>`
-    )
+  it('should render the heading', () => {
+    const { container, getByRole } = render(`<polymer3-app></polymer3-app>`)
 
     expect(
       getByRole('heading', { name: /polymer3 playground/i })
     ).toBeInTheDocument()
 
-    expect(container?.firstElementChild).toMatchSnapshot()
+    expect(container).toMatchSnapshot()
   })
 })
--- src/utils/tests/helpers.ts
+++ src/utils/tests/helpers.ts
@@ -1,13 +1,33 @@
-import { fixture } from '@open-wc/testing'
 import { within } from '@testing-library/dom'
 
-export const render = async (template: string) => {
-  const { shadowRoot: container } = await fixture<HTMLElement>(template)
+type Template = string
+
+const fixture = (template: Template): Element => {
+  const wrapper = document.createElement('div')
+  document.body.appendChild(wrapper)
+
+  wrapper.innerHTML = template
+  const [element] = wrapper.children
+  if (!element.shadowRoot) {
+    throw new Error('Template not found')
+  }
+
+  return element
+}
+
+const getContainer = ({ shadowRoot, tagName }: Element): HTMLElement => {
+  const wrapper = document.createElement(tagName)
+  wrapper.innerHTML = shadowRoot!.innerHTML
+  return wrapper
+}
+
+export const render = (template: Template) => {
+  const element = fixture(template)
   // @ts-ignore
-  const queries = within(container)
+  const queries = within(element.shadowRoot)
 
   return {
     ...queries,
-    container
+    container: getContainer(element)
   }
 }
