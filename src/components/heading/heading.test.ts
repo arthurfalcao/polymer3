cd6151a27d3997eeacdb94d557335dfd8030ee83
Arthur Falcão <arthurjvfalcao@gmail.com>
2021-04-06 00:52:41 +0100

    🎨 rename p3-heading to app-heading

--- src/components/heading/heading.test.ts
+++ src/components/heading/heading.test.ts
@@ -2,16 +2,16 @@ import { render } from '../../utils/tests/helpers'
 
 import '.'
 
-describe('<p3-heading />', () => {
+describe('<app-heading />', () => {
   it('should render the content', () => {
-    const { getByRole } = render(`<p3-heading>Polymer3</p3-heading>`)
+    const { getByRole } = render(`<app-heading>Polymer3</app-heading>`)
     expect(
       getByRole('heading', { name: /polymer3/i, level: 2 })
     ).toBeInTheDocument()
   })
 
   it('should render a black heading by default', () => {
-    const { getByRole } = render(`<p3-heading>Polymer3</p3-heading>`)
+    const { getByRole } = render(`<app-heading>Polymer3</app-heading>`)
     expect(getByRole('heading', { name: /polymer3/i })).toHaveClass(
       'text-black'
     )
@@ -19,7 +19,7 @@ describe('<p3-heading />', () => {
 
   it('should render a white heading when color is passed', () => {
     const { getByRole } = render(
-      `<p3-heading color="white">Polymer3</p3-heading>`
+      `<app-heading color="white">Polymer3</app-heading>`
     )
     expect(getByRole('heading', { name: /polymer3/i })).toHaveClass(
       'text-white'
@@ -27,7 +27,7 @@ describe('<p3-heading />', () => {
   })
 
   it('should render as a h3', () => {
-    const { getByRole } = render(`<p3-heading as="h3">Polymer3</p3-heading>`)
+    const { getByRole } = render(`<app-heading as="h3">Polymer3</app-heading>`)
     expect(getByRole('heading', { name: /polymer3/i, level: 3 }))
   })
 })
