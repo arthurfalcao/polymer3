a67ce3a2d2e2a697f25f0539da121f1e7790d46e
Arthur Falcão <arthurjvfalcao@gmail.com>
2021-04-06 21:00:06 +0100

    💄 add more styles to heading

--- src/components/heading/heading.ts
+++ src/components/heading/heading.ts
@@ -1,15 +1,17 @@
 import { property } from '@polymer/decorators'
 import { PolymerElement, html } from '@polymer/polymer'
 import { StampedTemplate } from '@polymer/polymer/interfaces'
+import classNames from 'classnames'
 
 import styles from './styles'
 
 type Color = 'black' | 'white'
-
+type Size = 'small' | 'medium'
 type As = keyof Pick<
   HTMLElementTagNameMap,
   'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 >
+type LineColor = 'primary' | 'secondary'
 
 class Heading extends PolymerElement {
   @property({ type: String })
@@ -18,11 +20,21 @@ class Heading extends PolymerElement {
   @property({ type: String })
   color: Color = 'black'
 
+  @property({ type: String })
+  size: Size = 'medium'
+
+  @property({ type: Boolean })
+  lineLeft = false
+
+  @property({ type: Boolean })
+  lineBottom = false
+
+  @property({ type: String })
+  lineColor: LineColor = 'primary'
+
   static get template() {
     return html`
-      <style>
-        ${styles}
-      </style>
+      ${styles}
 
       <slot></slot>
     `
@@ -32,7 +44,7 @@ class Heading extends PolymerElement {
    * Generetes the heading tag based on the `as` property
    */
   _attachDom(dom: StampedTemplate | null): ShadowRoot | null {
-    const { as, color } = this
+    const { as, color, size, lineLeft, lineBottom, lineColor } = this
 
     if (dom) {
       const slot = dom.querySelector('slot')
@@ -40,7 +52,12 @@ class Heading extends PolymerElement {
 
       if (slot) heading.appendChild(slot)
 
-      heading.classList.add(`text-${color}`)
+      const classes = classNames(`color-${color}`, `size-${size}`, {
+        'line-left': lineLeft,
+        'line-bottom': lineBottom,
+        [`line-color-${lineColor}`]: lineLeft || lineBottom
+      })
+      heading.classList.add(...classes.split(' '))
       dom.appendChild(heading)
     }
 
