9d547a37643875f6b2a8e99f4541e8058b55c654
Arthur Falcão <arthurjvfalcao@gmail.com>
2021-04-12 08:54:32 +0100

    ✅ add modulePaths to fix absolute import

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/styles/**/*.ts'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
   transformIgnorePatterns: []
 }
