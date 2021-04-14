52efeaba20d51983742534d182e6e3b0cb913dc9
Arthur Falcão <arthurjvfalcao@gmail.com>
2021-04-14 21:14:21 +0100

    🐛 remove fastDomIf

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,9 +1,7 @@
 import {
-  setFastDomIf,
   setLegacyWarnings,
   setRemoveNestedTemplates
 } from '@polymer/polymer/lib/utils/settings'
 
-setFastDomIf(true)
 setLegacyWarnings(true)
 setRemoveNestedTemplates(true)
